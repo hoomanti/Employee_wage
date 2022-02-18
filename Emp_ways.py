3421df7c441e43fd6d05da0183054be26eda364a
hoomanti <manisha.edu@protonmail.com>
2022-02-19 00:18:40 +0530

    Calculate daily employee wage

--- Emp_ways.py
+++ Emp_ways.py
@@ -1,8 +1,15 @@
 import random
 
+WAGE_PER_HOUR = 20
+FULL_DAY_HOUR = 8
+
 attendance = random.randint(0, 1)
 if attendance == 0:
     print("Employee is Absent")
+    daily_wage = 0
 else:
     print("Employee is Present")
+    daily_wage = WAGE_PER_HOUR * FULL_DAY_HOUR
+
+print("Daily wage of the Employee:", daily_wage)
 
