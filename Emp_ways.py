f2003acc8c7b2c182c4e20f77a1f03a794cd6c12
hoomanti <manisha.edu@protonmail.com>
2022-02-19 00:28:33 +0530

    Calculating wages for a month

--- Emp_ways.py
+++ Emp_ways.py
@@ -3,17 +3,26 @@ import random
 WAGE_PER_HOUR = 20
 FULL_DAY_HOUR = 8
 PART_TIME_HOUR = 6
+WORKING_DAY_PER_MONTH = 20
+WORKING_HOUR_PER_MONTH = 100
 
-attendance = random.randint(0, 2)
-if attendance == 0:
-    print("Employee is Absent")
-    daily_wage = 0
-elif attendance == 1:
-    print("Employee is Part time")
-    daily_wage = WAGE_PER_HOUR * PART_TIME_HOUR
-else:
-    print("Employee is Full time")
-    daily_wage = WAGE_PER_HOUR * FULL_DAY_HOUR
+day = 0
+hour = 0
+monthly_wage = 0
+while (day < WORKING_DAY_PER_MONTH) and (hour < WORKING_HOUR_PER_MONTH):
+    attendance = random.randint(0, 1)
+    if attendance != 0:
+        print("Employee is Full time")
+        daily_wage = WAGE_PER_HOUR * FULL_DAY_HOUR
+        hour += 8
+    else:
+        print("Employee is Part time")
+        daily_wage = WAGE_PER_HOUR * PART_TIME_HOUR
+        hour += 6
 
-print("Daily wage of the Employee:", daily_wage)
-
+    print("Daily wage of the Employee:", daily_wage)
+    day += 1
+    monthly_wage = monthly_wage + daily_wage
+print("Monthly wage is sum of above daily wages:", monthly_wage)
+print("Total working days:", day)
+print("Total working hours:", hour)
