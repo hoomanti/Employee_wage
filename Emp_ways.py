76aca1f675dd3067b68aaf0bf1c0bb03629bed6f
hoomanti <manisha.edu@protonmail.com>
2022-02-19 00:23:44 +0530

    Add part time employee and wage

--- Emp_ways.py
+++ Emp_ways.py
@@ -2,13 +2,17 @@ import random
 
 WAGE_PER_HOUR = 20
 FULL_DAY_HOUR = 8
+PART_TIME_HOUR = 6
 
-attendance = random.randint(0, 1)
+attendance = random.randint(0, 2)
 if attendance == 0:
     print("Employee is Absent")
     daily_wage = 0
+elif attendance == 1:
+    print("Employee is Part time")
+    daily_wage = WAGE_PER_HOUR * PART_TIME_HOUR
 else:
-    print("Employee is Present")
+    print("Employee is Full time")
     daily_wage = WAGE_PER_HOUR * FULL_DAY_HOUR
 
 print("Daily wage of the Employee:", daily_wage)
